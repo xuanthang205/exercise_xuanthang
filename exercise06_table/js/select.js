3001ce45c4c7794c1deec3cd2223149a6461e2f3
쑤언 탕_Xuan Thang <thangdx@hivelab.com>
2025-06-03 17:04:34 +0700

    Fix bug 5

---
+++ exercise06_table/js/select.js
@@ -0,0 +1,32 @@
+const selectItems = document.querySelectorAll('.select .select_item');
+const optionBtn = document.querySelector('.select .btn_option');
+const iconSelect = document.querySelector('.select .icon_arrow_down');
+const options = document.querySelector('.select_option');
+
+optionBtn.textContent = document.querySelector('.select_item.is_active .select_link').textContent;
+
+optionBtn.addEventListener('click', () => {
+  options.classList.toggle('is_show');
+  optionBtn.classList.toggle('is_show');
+});
+// Handling when selecting items
+selectItems.forEach(function (item) {
+  item.addEventListener('click', function () {
+    const selectedText = this.querySelector('.select_link').textContent;
+    // Remove old is_active
+    document.querySelector('.select .is_active')?.classList.remove('is_active');
+    // Assign new is_active
+    this.classList.add('is_active');
+    // Update main button content
+    optionBtn.textContent = selectedText;
+    optionBtn.classList.remove('is_show');
+    options.classList.remove('is_show');
+  });
+});
+
+window.addEventListener('resize', function () {
+  if (window.innerWidth > 768) {
+    optionBtn.classList.remove('is_show');
+    options.classList.remove('is_show');
+  }
+});
